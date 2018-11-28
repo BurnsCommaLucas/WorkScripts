84df55df745628351c5b57e4cce0f0dd4ce2ea9b
burnscommalucas <lburns534@gmail.com>
2018-11-28 12:41:04 -0800

    Updated to handle more than one file at a time

--- active.sh
+++ active.sh
@@ -1,6 +1,7 @@
 #!/bin/ksh
 
 # Safely move things 
+# Now supports multiple input files and handles paths which end with a "/"
 
 USAGE() { echo "Usage: $0 (-a|-d|-s|-r) [-c copy] [-h help]"; exit 0; }
 
@@ -13,23 +14,6 @@ SPATH="${RPATH}scripts/"
 DEST=""		# destination for the file we want to move
 CMD="mv"	# default to move file, rather than copy
 PCHK=false	# flag to update permissions
-
-# get last part of path in last argument
-for i in $@; do ; done
-i=`echo $i | grep -oe "[^\/]\+\/\?$"`
-
-# I uh, don't remember what this is supposed to do...
-if [ "$i" = "" ]
-then
-	i=`echo $i | awk -F '/' '{print $0}'`
-	echo $i
-fi
-
-# Check if current directory matches /comdsk/burnsl
-if [[ "${PWD}/${i}" = *"${RPATH}"* ]]
-then
-	; # There was some reason I wanted to know this but honestly I don't know why
-fi
 
 while getopts "cadsrh" OPTION
 do	
@@ -63,21 +47,48 @@ then
 	USAGE	
 fi
 
-# If file already exists at destination, ask about overwriting (default to not overwriting)
-if [ -e ${DEST}${i} ]
-then
-	echo -n "$i already exists in ${DEST} Overwrite? y/[n]: "
-	read LINE
-	if [ "${LINE}" = 'y' -o "${LINE}" = 'Y' ]
+for i in "$@"
+do
+	# skip flags (-a, -r, etc.)
+	if [ `echo ${i} | grep "\-\w*"` ]; then; continue; fi;
+	
+	# "safely" rip last parts of each path given.
+	# if string corresponds to a file/directory:
+	if [ -e "${i}" ] || [ -d "${i}" ]
 	then
-		;
-	else
-		exit 0
+		# Grab last segment of path
+		TMP=`echo $i | awk -F '/' '{print $NF}'`
+		if [ -z "${TMP}" ]
+		then
+			# or "second to last" if path ends with a "/"
+			TMP=`echo $i | awk -F '/' '{print $(NF-1)}'`
+			if [ -z "${TMP}" ]; then; continue; fi;
+		fi
+		i="${TMP}"
 	fi
-fi	
-
-# copy, and if we need to check permission, just smash them into place 
-if $CMD $i $DEST && [ $PCHK = true ]
-then
-	chmod -R 777 ${DEST}${i} 
-fi
+		
+	# Check if current source matches /comdsk/burnsl (this can probably be changed to a grep)
+	if [[ "${PWD}/${i}" = *"${RPATH}"* ]]
+	then
+		; # There was some reason I wanted to know this but honestly I don't know why
+	fi
+		
+	# If file already exists at destination, ask about overwriting (default to not overwriting)
+	if [ -e ${DEST}${i} ]
+	then
+		echo -n "$i already exists in ${DEST} Overwrite? y/[n]: "
+		read LINE
+		if [ "${LINE}" = 'y' -o "${LINE}" = 'Y' ]
+		then
+			;
+		else
+			exit 0
+		fi
+	fi	
+	
+	# copy, and if we need to check permission, just smash them into place 
+	if $CMD $i $DEST && [ $PCHK = true ]
+	then
+		chmod -R 777 ${DEST}${i} 
+	fi
+done
