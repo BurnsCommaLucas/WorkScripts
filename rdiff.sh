4098042469749aa034075ac49f00261971747ba2
burnscommalucas <lburns534@gmail.com>
2019-10-04 09:59:59 -0700

    Piped rdiff to more, removed spring tool which was part of spring (oops)

--- rdiff.sh
+++ rdiff.sh
@@ -2,4 +2,4 @@
 
 # I to ignore header changes which just clutter the diff when you lock a file
 
-diff -I '\$Header:.*\.' -X /comdsk/burnsl/scripts/exclude.txt -br ./ ./vs
+diff -I '\$Header:.*\.' -X /comdsk/burnsl/scripts/exclude.txt -br ./ ./vs | more
